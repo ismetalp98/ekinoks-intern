1972c7fca332b8d41a8954fa0d683cf9f653f277
İsmet Alp Eren <alp.eren@ug.bilkent.edu.tr>
2022-07-02 19:31:47 +0300

    make it better

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -23,15 +23,14 @@ export class KnexDB {
     init() {
         return new Promise((resolve, reject) => {
             try {
-                console.log(config.DATABASE_URL);
-                const result = JSON.stringify(this.db.migrate.latest());
-                console.log('Migration completed' + result);
+                // const result = JSON.stringify(this.db.migrate.latest());
+                // console.log('Migration completed' + result);
                 // this.db.seed.run();
                 resolve(true);
             } catch (error) {
-                const rollback = JSON.stringify(this.db.migrate.rollback());
-                console.log('Migration error' + error);
-                console.log('Migration error' + rollback);
+                // const rollback = JSON.stringify(this.db.migrate.rollback());
+                // console.log('Migration error' + error);
+                // console.log('Migration error' + rollback);
                 reject(false);
             }
             resolve(true);
